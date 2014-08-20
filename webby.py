034e167a0d0e987cf60f423e1b3d34d965e4babe
StephenHugo <stephenhugo@gmail.com>
2014-08-19 21:56:58 -0400

    init

--- webby.py
+++ webby.py
@@ -5,12 +5,24 @@ from flask import Blueprint
 app = Flask(__name__)
 
 home = Blueprint('home', __name__)
+conway = Blueprint('conway', __name__)
+about = Blueprint('about', __name__)
 
 @home.route('/')
 def index():
     return render_template('index.html')
 	 
+@conway.route('/conway')
+def index():
+    return render_template('conway.html')
+	 
+@about.route('/about')
+def index():
+    return render_template('about.html')
+	 
 app.register_blueprint(home)
+app.register_blueprint(conway)
+app.register_blueprint(about)
 
 if __name__ == '__main__':
 	app.debug = True
